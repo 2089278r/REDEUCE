a96dccd1d61cf8772e9848d58ac57b27614f5a89
Rasmus <2089278r@student.gla.ac.uk>
2016-07-07 12:40:19 +0100

    Not sure the notes made it last time...

--- src/uk/ac/glasgow/redeuce/peripherals/memory/DEUCECardReader.java
+++ src/uk/ac/glasgow/redeuce/peripherals/memory/DEUCECardReader.java
@@ -7,6 +7,7 @@ public class DEUCECardReader {
 	Card[] cardsBeingRead;  //Something to simulate the different states the reader can be in?
 	boolean isEmpty;
 	boolean readyToRead;    //Methods from Console, and when instructions are being executed can manipulate this
+	boolean isReading;
 	
 	public DEUCECardReader(){
 		this.cardsBeingRead = new Card[3];
@@ -21,8 +22,6 @@ public class DEUCECardReader {
 	
 	//The actual reading bit, I assume?
 	//Should this read 3 cards at a time(unless an interrupt from Console..?)
-	//and return an Array which is then sent to a Delay Line?
-	//This function I suppose should be called by Console?
 	public Word[] interpretCards(){
 		//while (!isEmpty() && !isWaiting())
 		    //getNextCard(x 3?)
